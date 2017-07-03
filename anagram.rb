a292761133409483b56f6d46657358ec572c47e1
Jarrod Li <admin@jarrodli.me>
2017-07-03 19:52:32 +1000

    Completed Anagram, todo: make a class?

--- anagram.rb
+++ anagram.rb
@@ -9,27 +9,31 @@ class Frequency
 	def initialize(string)
 		@string = string
 	end
-	def build(string)
+	def build
 		newH = Hash.new(0)
 		string.each_char { |c| newH[c] += 1 } 
 		newH
 	end
 end
 
-class Anagram < Frequency
+=begin class Anagram < Frequency
 	def initialize(s1, s2)
 		@s1 = s1
 		@s2 = s2
 	end
 	def check
-		return true if @s1.build = @s2.build
+		return true if @s1.build == @s2.build
 	end
 end
-
-t = Anagram.new("hello", "olleh")
-l = t.check
-puts l
+=end
 
 #t = Frequency.new("string")
 #l = t.build
 #l.each { |x, y| puts "#{x}: #{y} times \n"}
+
+
+def isAnagram(str1, str2) 
+	Frequency.new(str1).build == Frequency.new(str2).build ? true : false
+end
+
+puts isAnagram(gets.chomp.to_s, gets.chomp.to_s)
